24e9d3cf38938bfb5e8e44bc6c7dbe65bb8f20c6
Lisa Louison <lisa.louison@gmail.com>
2025-07-29 14:49:50 -0500

    feat(recipe router): create recipe router

---
+++ server/routes/recipes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import recipeController from '../controllers/recipeController';
+
+const router = express.Router();
+
+router.get('/', recipeController.getAllRecipes, (_req, res) => {
+  res.status(200).send({ recipes: res.locals.recipes });
+});
+
+router.get('/:id', recipeController.getOneRecipe, (_req, res) => {
+  res.status(200).send({ recipe: res.locals.recipe });
+});
+
+router.post('/', recipeController.createRecipe, (_req, res) => {
+  res.status(200).send({ newRecipe: res.locals.recipe });
+});
+
+router.put('/:id', recipeController.updateOneRecipe, (_req, res) => {
+  res.status(200).send({ updatedRecipe: res.locals.recipe });
+});
+
+router.delete('/:id', recipeController.deleteOneRecipe, (_req, res) => {
+  res.status(200).send({ deletedRecipe: res.locals.recipe });
+});
