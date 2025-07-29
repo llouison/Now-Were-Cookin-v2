05e70384996e4b425b488315d7ceb0dd7402dcbb
Lisa Louison <lisa.louison@gmail.com>
2025-07-29 11:03:26 -0500

    feat(server): create basic server

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = 3000;
+
+/* Automatically parse urlencoded body content and form
+data from incoming requests and place it in req.body */
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (_req, res) => {
+  res.sendFile(
+    path.resolve(path.resolve(import.meta.dirname, '../index.html'))
+  );
+});
+
+/* 404 handler - TO-DO: Create custom page */
+app.use('*catchall', (_req, res) => {
+  res.status(404).send('Not Found');
+});
+
+/* Global error handler */
+app.use((err, _req, res, _next) => {
+  console.log(err);
+  res.status(500).send({ error: err });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 express server listening on port ${PORT}`);
+});
+
+// module.exports = app;
