a5e89300fecc39e17b814ab38df1c9db89d11397
Lisa Louison <lisa.louison@gmail.com>
2025-07-29 11:47:52 -0500

    feat(connect db): install mongoose and dot env dependencies to connect to database

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+  try {
+    console.log('mongo uri is:', process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    //app will shut down if err
+    process.exit(1);
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import path from 'path';
+// import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 
+dotenv.config();
 const app = express();
 const PORT = 3000;
+
+// mongoose.connect(process.env.MONGO_URI);
 
 /* Automatically parse urlencoded body content and form
 data from incoming requests and place it in req.body */
@@ -27,6 +33,7 @@ app.use((err, _req, res, _next) => {
 });
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`🚀 express server listening on port ${PORT}`);
 });
 
