14b742a64a618fc3a5dbebb008026a5accf27795
Lisa Louison <lisa.louison@gmail.com>
2025-07-29 18:55:29 -0500

    refactor(async): refactor controller mthods to async await

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,55 +2,50 @@ import User from '../models/userModel.js';
 
 const userController = {};
 
-/**
- * createUser - create and save a new User into the database.
- */
-userController.createUser = (req, res, next) => {
-  // write code here
+userController.createUser = async (req, res, next) => {
   const { username, password } = req.body;
-  User.create({
-    username: username,
-    password: password,
-  })
-    .then((data) => {
-      //   console.log(`💁🏽 userController.createUser: data: ${data}`);
-      res.locals.user = data;
-      return next();
-    })
-    .catch((err) => {
-      return next({
-        log: `Error caught in userController.createUser: ${err}`,
-        message: {
-          err: 'Could not create new user! See log for details',
-        },
-      });
+  const userExists = await User.findOne({ username: username });
+  try {
+    if (userExists) {
+      throw new Error('user already exists');
+    }
+    const user = await User.create({
+      username: username,
+      password: password,
     });
+    const userData = await user;
+    res.locals.user = userData;
+    next();
+  } catch (err) {
+    return next({
+      log: `Error caught in userController.createUser: ${err}`,
+      message: {
+        err: 'Could not create new user! See log for details',
+      },
+    });
+  }
 };
 
-/**
- * verifyUser - Obtain username and password from the request body, locate
- * the appropriate user in the database, and then authenticate the submitted password
- * against the password stored in the database.
- */
-userController.verifyUser = (req, res, next) => {
+userController.verifyUser = async (req, res, next) => {
   const { username, password } = req.body;
-  User.findOne({
-    username: username,
-  })
-    .exec()
-    .then(async (user) => {
-      if (!data || !(await user.comparePasswords(password))) {
-        res.locals.loggedIn = false;
-      } else {
-        console.log(`🔍 successful user Obj from mongo: ${data}`);
-        res.locals.loggedIn = true;
-        res.locals.user = user;
-      }
-      next();
-    })
-    .catch((err) => {
-      console.log(`An error has occurred: ${err}`);
+  const userExists = await User.findOne({ username: username });
+  try {
+    if (!userExists || !(await User.comparePasswords(password))) {
+      res.locals.loggedIn = false;
+    } else {
+      console.log(`🔍 successful user Obj from mongo: ${userExists}`);
+      res.locals.loggedIn = true;
+      res.locals.user = userExists;
+    }
+    next();
+  } catch (err) {
+    return next({
+      log: `Error caught in userController.verifyUser: ${err}`,
+      message: {
+        err: 'Could not verify new user! See log for details',
+      },
     });
+  }
 };
 
 export default userController;
