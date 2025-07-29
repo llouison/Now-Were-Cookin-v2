e06855090255d0b13192b559a1e6ff21dae4d1e7
Lisa Louison <lisa.louison@gmail.com>
2025-07-29 16:53:43 -0500

    feat(protect): declare protected routes

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -3,7 +3,7 @@ import recipeController from '../controllers/recipeController.js';
 
 const router = express.Router();
 
-router.get('/', recipeController.getAllRecipes, (_req, res) => {
+router.get('/', protect, recipeController.getAllRecipes, (_req, res) => {
   res.status(200).send({ recipes: res.locals.recipes });
 });
 
@@ -15,12 +15,17 @@ router.post('/', recipeController.createRecipe, (_req, res) => {
   res.status(200).send({ newRecipe: res.locals.recipe });
 });
 
-router.put('/:id', recipeController.updateOneRecipe, (_req, res) => {
+router.put('/:id', protect, recipeController.updateOneRecipe, (_req, res) => {
   res.status(200).send({ updatedRecipe: res.locals.recipe });
 });
 
-router.delete('/:id', recipeController.deleteOneRecipe, (_req, res) => {
-  res.status(200).send({ deletedRecipe: res.locals.recipe });
-});
+router.delete(
+  '/:id',
+  protect,
+  recipeController.deleteOneRecipe,
+  (_req, res) => {
+    res.status(200).send({ deletedRecipe: res.locals.recipe });
+  }
+);
 
 export default router;
