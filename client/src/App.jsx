c205212fba5bd12817d25c145fbe3260c19ed96c
Lisa Louison <lisa.louison@gmail.com>
2025-07-30 11:59:44 -0500

    feat(routes): import routes to conditionally render components based on url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from 'react-router';
 import Navbar from './components/Navbar';
 import { AuthProvider } from './context/AuthContext';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 const App = () => {
   return (
     <AuthProvider>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </AuthProvider>
   );
 };
