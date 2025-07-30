d33f5e7d409cbf486b78fb61c8c80fe6f8ec343b
Lisa Louison <lisa.louison@gmail.com>
2025-07-30 12:37:17 -0500

    feat(signup): update component to submit signup form

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router';
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signup } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signup(username, password);
+      navigate('/');
+    } catch (err) {
+      console.error('Signup error:', err);
+    }
+  };
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            name='username'
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            name='password'
+          />
+        </div>
+        <button type='submit'>Signup</button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
