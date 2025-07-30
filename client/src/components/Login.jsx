37a19c01ce250c99865e6c62e061797002b58036
Lisa Louison <lisa.louison@gmail.com>
2025-07-30 12:40:59 -0500

    feat(login): update component to submit login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await login(username, password);
+      navigate('/');
+    } catch (err) {
+      console.error('Login error:', err);
+    }
+  };
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username</label>
+          <input
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            name='username'
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <input
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            name='password'
+          />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
