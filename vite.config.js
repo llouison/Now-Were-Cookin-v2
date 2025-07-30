9503be6c74c6aadc75dc5855b1ce8aebd6953990
Lisa Louison <lisa.louison@gmail.com>
2025-07-29 21:24:01 -0500

    fix(proxy): fix proxy route in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/': 'http://localhost:3000',
     },
   },
 });
