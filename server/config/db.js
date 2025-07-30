f9feeb6e01b0a7fd9c5ee682db4d863de7fc6145
Lisa Louison <lisa.louison@gmail.com>
2025-07-29 20:48:47 -0500

    refactor(logs): remove unnecessary console log

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    console.log('mongo uri is:', process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`🗄️  MongoDB connected!`);
   } catch (err) {
