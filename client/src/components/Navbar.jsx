f0aeacca9cffd5b4086659c9200e511256493d50
Lisa Louison <lisa.louison@gmail.com>
2025-07-30 11:45:07 -0500

    feat(nav): create navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h1>Recipes</h1>
+        <div>
+          <button>Login</button>
+          <button>Signup</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
