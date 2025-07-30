6172ac53a07109e8153902c78eecacbdd92e2b01
Lisa Louison <lisa.louison@gmail.com>
2025-07-30 12:47:52 -0500

    feat(logout): add logout functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+  const { user, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
   return (
     <nav>
       <div>
@@ -10,12 +18,20 @@ const Navbar = () => {
         </Link>
 
         <div>
-          <Link to='/login'>
-            <button>Login</button>
-          </Link>
-          <Link to='/signup'>
-            <button>Signup</button>
-          </Link>
+          {user ? (
+            <div>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) : (
+            <>
+              <Link to='/login'>
+                <button>Login</button>
+              </Link>
+              <Link to='/signup'>
+                <button>Signup</button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
